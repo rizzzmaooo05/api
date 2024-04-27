5ad429ddcc6eb29c14b9d633bc521b5003f320f4
Rizqon Maulana <158800768+rizzzmaooo05@users.noreply.github.com>
2024-04-27 20:44:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { createClient } from '@supabase/supabase-js'
+
+const app = express()
+const port = 6969
+
+const supabaseUrl = 'https://yoaooonjlnahhcgxdyxr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlvYW9vb25qbG5haGhjZ3hkeXhyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNDIxMjE3MSwiZXhwIjoyMDI5Nzg4MTcxfQ.os_Y6SoXOUOx1rZ1QjAvkVdx_Dx9Yh1Mou2uM4yBPKk'
+const db = createClient(supabaseUrl, supabaseKey)
+
+app.get('/', async (req, res) => {
+  const getData = await db.from('books').select()
+
+  res.send(getData.data)
+})
+
+app.listen(port, () => {
+  console.log('server running in port ' + port)
+})
